0d54f42bdc884b31055b56089aed0e05a008a6f8
arvindsaini2000 <142617949+arvindsaini2000@users.noreply.github.com>
2026-07-18 22:04:15 +0530

    Remove floor details from unit plan

--- dist/assets/AboutUs-C5_Z4d5d.js
+++ dist/assets/AboutUs-C5_Z4d5d.js
@@ -1,4 +1,4 @@
-import{R as e,m as a}from"./index-MKoZ0ebM.js";import{c as i,B as o,U as c,S as d,b as p,A as s,H as h,M as m,P as E,a as g}from"./users-BPGSEo7j.js";/**
+import{R as e,m as a}from"./index-CJfNV3f-.js";import{c as i,B as o,U as c,S as d,b as p,A as s,H as h,M as m,P as E,a as g}from"./users-DJtSO5kf.js";/**
  * @license lucide-react v1.24.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- dist/assets/users-DJtSO5kf.js
+++ dist/assets/users-DJtSO5kf.js
@@ -1,4 +1,4 @@
-import{r as s}from"./index-MKoZ0ebM.js";/**
+import{r as s}from"./index-CJfNV3f-.js";/**
  * @license lucide-react v1.24.0 - ISC
  *
  * This source code is licensed under the ISC license.
