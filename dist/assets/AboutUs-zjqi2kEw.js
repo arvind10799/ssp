7b03acd69f3f88ca8a6a38b1b61b8423dda9911c
arvindsaini2000 <142617949+arvindsaini2000@users.noreply.github.com>
2026-07-18 20:17:09 +0530

    Add mobile 720p hero video

--- dist/assets/AboutUs-zjqi2kEw.js
+++ dist/assets/AboutUs-zjqi2kEw.js
@@ -1,4 +1,4 @@
-import{R as e,m as a}from"./index-X2xs9vFE.js";import{c as i,B as o,U as c,S as d,b as p,A as s,H as h,M as m,P as E,a as g}from"./users-D_boAvlx.js";/**
+import{R as e,m as a}from"./index-BKWIdWxt.js";import{c as i,B as o,U as c,S as d,b as p,A as s,H as h,M as m,P as E,a as g}from"./users-CH-yYd3m.js";/**
  * @license lucide-react v1.24.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- dist/assets/users-CH-yYd3m.js
+++ dist/assets/users-CH-yYd3m.js
@@ -1,4 +1,4 @@
-import{r as s}from"./index-X2xs9vFE.js";/**
+import{r as s}from"./index-BKWIdWxt.js";/**
  * @license lucide-react v1.24.0 - ISC
  *
  * This source code is licensed under the ISC license.
