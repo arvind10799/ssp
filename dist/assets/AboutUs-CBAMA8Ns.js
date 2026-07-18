553a73ad22b58078732d7dd4f286d2f8b1ee20d4
arvindsaini2000 <142617949+arvindsaini2000@users.noreply.github.com>
2026-07-18 19:28:56 +0530

    Restore full AR Belle Vie hero video

--- dist/assets/AboutUs-CBAMA8Ns.js
+++ dist/assets/AboutUs-CBAMA8Ns.js
@@ -1,4 +1,4 @@
-import{R as e,m as a}from"./index-W9I-o1FA.js";import{c as i,B as o,U as c,S as d,b as p,A as s,H as h,M as m,P as E,a as g}from"./users-BmME4pCa.js";/**
+import{R as e,m as a}from"./index-Db6-p_Hb.js";import{c as i,B as o,U as c,S as d,b as p,A as s,H as h,M as m,P as E,a as g}from"./users-Diy1Jjej.js";/**
  * @license lucide-react v1.24.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- dist/assets/users-Diy1Jjej.js
+++ dist/assets/users-Diy1Jjej.js
@@ -1,4 +1,4 @@
-import{r as s}from"./index-W9I-o1FA.js";/**
+import{r as s}from"./index-Db6-p_Hb.js";/**
  * @license lucide-react v1.24.0 - ISC
  *
  * This source code is licensed under the ISC license.
