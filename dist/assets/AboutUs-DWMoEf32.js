d8d1b57756f0cb9a3035dc43327b8661283a6b14
arvindsaini2000 <142617949+arvindsaini2000@users.noreply.github.com>
2026-07-20 03:29:00 +0530

    Update AR Belle Vie floor plan table

--- dist/assets/AboutUs-DWMoEf32.js
+++ dist/assets/AboutUs-DWMoEf32.js
@@ -1,4 +1,4 @@
-import{R as e,m as a}from"./index-DxIJtiua.js";import{c as i,B as o,U as c,S as d,b as p,A as s,H as h,M as m,P as E,a as g}from"./users-B0r2SJtm.js";/**
+import{R as e,m as a}from"./index-BULre0Gd.js";import{c as i,B as o,U as c,S as d,b as p,A as s,H as h,M as m,P as E,a as g}from"./users-whPqSoL6.js";/**
  * @license lucide-react v1.24.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- dist/assets/users-whPqSoL6.js
+++ dist/assets/users-whPqSoL6.js
@@ -1,4 +1,4 @@
-import{r as s}from"./index-DxIJtiua.js";/**
+import{r as s}from"./index-BULre0Gd.js";/**
  * @license lucide-react v1.24.0 - ISC
  *
  * This source code is licensed under the ISC license.
