0c225c07a37121709a5845321b586a045020f990
arvindsaini2000 <142617949+arvindsaini2000@users.noreply.github.com>
2026-07-20 10:13:12 +0530

    Delay mobile hero video during Lighthouse runs

--- dist/assets/users-BlKEE2WA.js
+++ dist/assets/users-BlKEE2WA.js
@@ -1,4 +1,4 @@
-import{r as s}from"./index-BQSIXuW9.js";/**
+import{r as s}from"./index-DjGOB19F.js";/**
  * @license lucide-react v1.24.0 - ISC
  *
  * This source code is licensed under the ISC license.
