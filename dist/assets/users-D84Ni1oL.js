8866e3222b54f83511006682631b87cb6a37612b
arvindsaini2000 <142617949+arvindsaini2000@users.noreply.github.com>
2026-07-20 10:53:11 +0530

    Fix AR Belle Vie mobile plans nav

--- dist/assets/users-D84Ni1oL.js
+++ dist/assets/users-D84Ni1oL.js
@@ -1,4 +1,4 @@
-import{r as s}from"./index-D_4GflOY.js";/**
+import{r as s}from"./index-zENfAa9J.js";/**
  * @license lucide-react v1.24.0 - ISC
  *
  * This source code is licensed under the ISC license.
