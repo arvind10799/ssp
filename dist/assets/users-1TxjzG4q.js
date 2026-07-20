836df47c22ae1a0ca37e98adde7c06e2fb158b86
arvindsaini2000 <142617949+arvindsaini2000@users.noreply.github.com>
2026-07-20 10:31:03 +0530

    Keep static AR hero through Lighthouse LCP

--- dist/assets/users-1TxjzG4q.js
+++ dist/assets/users-1TxjzG4q.js
@@ -1,4 +1,4 @@
-import{r as s}from"./index-DjGOB19F.js";/**
+import{r as s}from"./index-D_4GflOY.js";/**
  * @license lucide-react v1.24.0 - ISC
  *
  * This source code is licensed under the ISC license.
