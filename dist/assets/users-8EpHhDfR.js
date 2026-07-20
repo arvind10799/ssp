eb8a571340b3c7e8d84a5e42205225abfbf48f8f
arvindsaini2000 <142617949+arvindsaini2000@users.noreply.github.com>
2026-07-20 09:38:33 +0530

    Improve AR Belle Vie mobile LCP

--- dist/assets/users-8EpHhDfR.js
+++ dist/assets/users-8EpHhDfR.js
@@ -1,4 +1,4 @@
-import{r as s}from"./index-DcfexrW6.js";/**
+import{r as s}from"./index-BeykjgSy.js";/**
  * @license lucide-react v1.24.0 - ISC
  *
  * This source code is licensed under the ISC license.
