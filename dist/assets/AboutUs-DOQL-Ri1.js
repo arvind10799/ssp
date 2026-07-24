acac80b88c539316b2bf071f51909ff3d826223b
arvindsaini2000 <142617949+arvindsaini2000@users.noreply.github.com>
2026-07-25 01:12:44 +0530

    Deploy AR Belle Vie overview updates

--- dist/assets/AboutUs-DOQL-Ri1.js
+++ dist/assets/AboutUs-DOQL-Ri1.js
@@ -1,4 +1,4 @@
-import{R as e,m as a}from"./index-N02GLcI-.js";import{c as i,B as o,U as c,S as d,b as p,A as s,H as h,M as m,P as E,a as g}from"./users-BpDBhIVP.js";/**
+import{R as e,m as a}from"./index-DaqtG1yS.js";import{c as i,B as o,U as c,S as d,b as p,A as s,H as h,M as m,P as E,a as g}from"./users-DMWgvQ0A.js";/**
  * @license lucide-react v1.24.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- dist/assets/users-DMWgvQ0A.js
+++ dist/assets/users-DMWgvQ0A.js
@@ -1,4 +1,4 @@
-import{r as s}from"./index-N02GLcI-.js";/**
+import{r as s}from"./index-DaqtG1yS.js";/**
  * @license lucide-react v1.24.0 - ISC
  *
  * This source code is licensed under the ISC license.
